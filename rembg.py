f7a6e4450b516de6dd113d85af53ade1bc1b0019
Patrick Römer <Patrick.Roemer@rtl.de>
2024-08-18 18:46:17 +0200

    aktueller Stand

---
+++ rembg.py
@@ -0,0 +1,21 @@
+
+from rembg import remove
+import typer
+from pathlib import Path
+
+def main(file_to_process: Path):
+    if not file_to_process.exists():
+        raise Exception("Der Pfad: "+file_to_process+" existiert nicht!")
+    
+    output_path = './background-removed/'+file_to_process.name
+
+    with open(file_to_process, 'rb') as i:
+        with open(output, 'wb') as o:
+            input = i.read()
+            output = remove(input)
+            o.write(output)
+
+
+if __name__ == "__main__":
+    print("Remove Background started...")
+    typer.run(main)
---
+++ setup-rembg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python3 -m pip install --upgrade pip &&
+sudo apt-get update && 
+pip install typer && 
+pip install rembg
